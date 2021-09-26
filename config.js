eccb4934493a1b79d755865d6bda17c963917b6f
clqu <76551996+clqu@users.noreply.github.com>
2021-09-26 14:54:15 +0300

    ✨ All files uploaded without any problems.

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    token: "",
+    statusCh: "", // Channel ID
+    checkSites: [
+        { id: 1, name: 'Website', link: 'https://vcodes.xyz' },
+        { id: 2, name: 'API', link: 'https://api.vcodes.xyz' },
+        { id: 3, name: 'Documents', link: 'https://docs.vcodes.xyz' }
+    ]
+}
---
+++ index.js
@@ -0,0 +1,75 @@
+const { Client, MessageEmbed } = require("discord.js");
+const request = require("request");
+const client = new Client();
+const config = require("./config");
+const db = require("quick.db");
+
+client.on("ready", async () => {
+  console.log("[#] Bot successfully connected as " + client.user.tag + ".");
+  client.user.setPresence({
+    activity: { type: "WATCHING", name: "the statuses | !services" },
+  });
+  const channel = client.channels.cache.get(config.statusCh);
+  setInterval(() => {
+    config.checkSites.forEach((cfg) => {
+      request(cfg.link, async function (error, response, body) {
+        if (response.statusCode >= 200 && response.statusCode < 300) {
+          if (db.has(`down.${cfg.id}`)) {
+            db.delete(`down.${cfg.id}`);
+            const activeAll = new MessageEmbed()
+              .setDescription(
+                `<:vcodesOperational:891428689844260906> **[${cfg.name}](${cfg.link}) are online.**`
+              )
+              .setColor("GREEN")
+              .setFooter("status.vcodes.xyz | by clqu");
+            channel.send(activeAll);
+          }
+        } else {
+          if (!db.has(`down.${cfg.id}`)) {
+            db.set(`down.${cfg.id}`, true);
+            const someDown = new MessageEmbed()
+              .setDescription(
+                `<:vcodesDowntime:891428689705861131> **[${cfg.name}](${cfg.link}) are down.**`
+              )
+              .setColor("RED")
+              .setFooter("status.vcodes.xyz | by clqu");
+            channel.send(someDown);
+          }
+        }
+      });
+    });
+  }, 5000);
+});
+
+client.on("message", (message) => {
+  if (message.content === "!services") {
+    const activeAll = new MessageEmbed()
+      .setDescription(
+        "<:vcodesOperational:891428689844260906> **All services are online.**"
+      )
+      .setColor("GREEN")
+      .setFooter("status.vcodes.xyz | by clqu");
+    const someDown = new MessageEmbed()
+      .setDescription(
+        "<:vcodesDowntime:891428689705861131> **Some services are down.**"
+      )
+      .setColor("RED")
+      .setFooter("status.vcodes.xyz | by clqu");
+    let downedServices = [];
+    config.checkSites.forEach((cfg) => {
+      if (db.has(`down.${cfg.id}`)) {
+        downedServices.push(cfg);
+      }
+    });
+    if (downedServices.length > 0) {
+      downedServices.map((serv) => {
+        someDown.addField(serv.name, serv.link);
+      });
+    }
+    message.channel.send(downedServices.length > 0 ? someDown : activeAll);
+  }
+  if (message.content === "!ping") {
+    message.reply("Pong... " + client.ws.ping + "ms");
+  }
+});
+client.login(config.token);
